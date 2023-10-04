55604b36631b61c66d58c0df45b5c8c85c03cf0f
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-04 13:22:42 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(379, 49)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(41, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(88, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter value of x"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(41, 143)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Value of r :"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(41, 96)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Enter value of Y"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(379, 88)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(379, 143)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim x As Double
+        Dim y As Double
+        Dim r As Double
+        x = Val(TextBox1.Text)
+        y = Val(TextBox2.Text)
+        r = (x ^ 2 + y ^ 2) ^ 0.5
+        TextBox3.Text = r
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As Double
+        Dim y As Double
+        Dim r As Double
+        x = Val(TextBox1.Text)
+        y = Val(TextBox2.Text)
+        r = (x ^ 2 + y ^ 2) ^ 0.5
+        TextBox3.Text = r.ToString("F2")
+    End Sub
+End Class
